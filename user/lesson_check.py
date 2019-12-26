934dae31f697bb23b79db88c7cb534e29a90d435
GitProger <51924999+GitProger@users.noreply.github.com>
2019-12-26 15:48:14 +0300

    Update lesson_check.py

--- user/lesson_check.py
+++ user/lesson_check.py
@@ -29,3 +29,80 @@ School = [
     Lesson(13, 40  ,  14, 20),
 ]
 
+
+class User:
+    def __init__(self, i):
+        self.uid = i
+        self.name = ''
+        self.total = 0
+        self.online = 0
+        self.now = False
+        
+villains = []
+
+pts_ids_list = [
+    #2022a
+     460838671, 221493415, 246178303, 415526037,
+     258691338, 266035392, 178101321, 167326970,
+     212694300, 257244889, 287018057, 387610057,
+     429317016, 418500776, 396305534, 247633967,
+     517859380, 351851366, 244962256, 289416361,
+     229801802, 195947948, 570392202, 572297054,    
+    #/2022a
+]
+
+
+
+class Robot:
+    def __init__(self, log: str, pas: str):
+        self.__session = requests.Session()
+        self.vk = vk_api.VkApi(log, pas, token)
+        self.vk.auth(token_only=True)
+
+    def __online(self, uid):
+        inf = self.__whois(uid, 'online')[0]
+        return inf['online']
+
+    def __many_online(self, uids):
+        inf = self.vk.method(
+            'users.get',
+            {
+                'user_ids': str(list(uids))[1:-1], #', '.join([str(x) for x in uids])
+                'fields': 'online'
+            }
+        )
+        res = dict()
+        for i in inf:
+            res[i['id']] = i['online']
+        return res
+
+    def __whois(self, uid, fields=''):
+        return self.vk.method(
+            'users.get',
+            {
+                'user_ids': uid,
+                'fields': fields
+            }
+        )
+
+    def __name(self, uid):
+        inf = self.__whois(uid)[0]
+        return inf['first_name'] + ' ' + inf['last_name']
+
+    def __friends(self, uid, fields=''):
+        return self.vk.method(
+            'friends.get',
+            {
+                'user_id': uid,
+                'fields': fields
+            }
+        )
+
+    def routine(self):
+        pass
+
+def main(args):
+    Robot(login, password).routine()
+    return 0
+if __name__ == '__main__':
+    exit(main(sys.argv))
