4dca48fead1f89a344198870d3c2f2adf08876f2
GitProger <51924999+GitProger@users.noreply.github.com>
2020-02-20 19:34:49 +0300

    Update whoonline.py

--- user/whoonline.py
+++ user/whoonline.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
 import sys, os, requests
-import vk_api, json, time
+import vk_api, json, time, datetime
+import math
 
-login = 'telephone_number_as_string'
+login = '+telephone'
 password = 'password'
+
+################################################################################
+################################################################################
+################################################################################
 
 def cur_date():
     return datetime.datetime.now().strftime("%d.%m.%Y %H:%M")
@@ -16,48 +21,64 @@ class User:
         self.total = 0
         self.online = 0
         self.now = False
+
 def make_list(users):
-    res = []
-    for u in users:
-        res.append(User(u))
-    return res
+    return [User(u) for u in users]
 
-class Robot:
-    # vk is a vk api session
-    @staticmethod
-    def jsonprint(jsn):
-        print(json.dumps(jsn, sort_keys=True, ensure_ascii=False, indent=4))
+def write_to_file(users, tm):
+    f = open('data.txt', 'w')
+    f.write(str(tm) + '\n')
+    for user in users:
+        f.write('{} {} {}\n'.format(user.uid, user.online, user.total))
+    f.close()
 
+def read_from_file():
+    f = open('data.txt', 'r')
+    users = []
+    tm = 0
+    ss = f.readlines()
+    tm = int(ss[0])
+    for i in range(1, len(ss)):
+        pars = ss[i].split()
+        while len(pars) < 3:
+            pars.append(0)
+        if pars[0] == 0:
+            break
+        users.append(User(int(pars[0])))
+        users[-1].online = int(pars[1])
+        users[-1].total = int(pars[2])
+    f.close()
+    return (tm, users)
+
+################################################################################
+################################################################################
+################################################################################
+
+
+class BasicRobot:
     def __init__(self, log: str, pas: str):
         self.__session = requests.Session()
-        self.vk = vk_api.VkApi(log, pas, token)
+        self.vk = vk_api.VkApi(log, pas)
         self.vk.auth(token_only=True)
 
-    #possible fields:
-    # nickname, screen_name, sex, bdate, city, country, timezone,
-    # photo_50, photo_100, photo_200_orig, has_mobile, contacts,
-    # education, online, counters, relation, last_seen, status,
-    # can_write_private_message, can_see_all_posts, can_post, universities
-    def __online(self, uid):
-        inf = self.__whois(uid, 'online')[0]
+    def _online(self, uid):
+        inf = self._whois(uid, 'online')[0]
         return inf['online']
 
-
-    def __many_online(self, uids):
+    def _many_online(self, uids):
         inf = self.vk.method(
             'users.get',
             {
-                'user_ids': str(list(uids))[1:-1],
+                'user_ids': str(list(uids))[1:-1], #', '.join([str(x) for x in uids])
                 'fields': 'online'
             }
         )
         res = dict()
         for i in inf:
             res[i['id']] = i['online']
-        return res 
+        return res
 
-
-    def __whois(self, uid, fields=''):
+    def _whois(self, uid, fields=''):
         return self.vk.method(
             'users.get',
             {
@@ -66,11 +87,11 @@ class Robot:
             }
         )
 
-    def __name(self, uid):
-        inf = self.__whois(uid)[0]
+    def _name(self, uid):
+        inf = self._whois(uid)[0]
         return inf['first_name'] + ' ' + inf['last_name']
 
-    def __friends(self, uid, fields=''):
+    def _friends(self, uid, fields=''):
         return self.vk.method(
             'friends.get',
             {
@@ -79,53 +100,77 @@ class Robot:
             }
         )
 
+################################################################################
+################################################################################
+################################################################################
+
+class MyRobot(BasicRobot):
     def routine(self):
-        watch = (
-            #ids
-	)
-        totaltime = 0
-        usrs = make_list(watch)
+        if 0 == 0:
+             watch = (
+                  460838671, 221493415, 246178303, 415526037,
+                  258691338, 266035392, 178101321, 167326970,
+                  212694300, 257244889, 287018057, 387610057,
+                  429317016, 418500776, 396305534, 247633967,
+                  517859380, 351851366, 244962256, 289416361,
+                  229801802, 195947948, 570392202, 572297054,
+             )
+             totaltime = 0
+             usrs = make_list(watch)
+        else:
+            totaltime, usrs = read_from_file()
+
+        INTERVAL = 5
         for user in usrs:
-            user.name = self.__name(user.uid)
+            user.name = self._name(user.uid)
+
+        print('Initialized.')
 
         while True:
-            time.sleep(60 * 5)
-            totaltime += 5
-            os.system('clear')
+            try:
+                time.sleep(60 * INTERVAL)
+                os.system('clear')
 
-            onlineinfo = self.__many_online(watch)
-            for user in usrs:
-                user.total += 5
-                if onlineinfo[user.uid] == 1: # self.__online(user.uid) == onlineinfo[user.uid]
-                    user.online += 5
-                    user.now = '*'
-                else:
-                    user.now = ' '
-                amnt = 100 * user.online / user.total
-                amnt = math.ceil(1000 * amnt) / 1000
+                totaltime += INTERVAL
+                users_online = 0
+                onlineinfo = self._many_online([u.uid for u in usrs])
+
+                for user in usrs:
+                    user.total += INTERVAL
+                    if onlineinfo[user.uid] == 1: # self.__online(user.uid)
+                        user.online += INTERVAL
+                        user.now = '*'
+                        users_online += 1
+                    else:
+                        user.now = ' '
+
+                    amnt = 100 * user.online / user.total
+                    amnt = math.ceil(1000 * amnt) / 1000
+                    print(
+                        user.name, (30 - len(user.name)) * ' ',
+                        user.now, ' : ',
+                        amnt, '%', (9 - len(str(amnt))) * ' ',
+                        'time online', sep=''
+                    )
+
+                print()
                 print(
-                    user.name, (30 - len(user.name)) * ' ' ,
-                    user.now, ' : ',
-                    amnt, '%', (9 - len(str(amnt))) * ' ',
-                    'time online', sep=''
+                    'Total time: ',
+                    totaltime // (24 * 60), 'd, ',
+                    (totaltime % (24 * 60)) // 60, 'h, ',
+                    totaltime % 60, 'm', sep=''
                 )
-            print()
-            print(
-                'Total time: ',
-                 totaltime // (24 * 60), 'd, ',
-                 (totaltime % (24 * 60)) // 60, 'h, ',
-                 totaltime % 60, 'm', sep=''
-            )
-            print('Now:', cur_date())
-            print('* - online users');
+                print('Now:', cur_date())
+                print('* - online users')
+                print(users_online, 'users online')
+
+            except:
+                write_to_file(usrs, totaltime)
+                return -1
 
 def main(args):
-    Robot(login, password).routine()
+    MyRobot(login, password).routine()
     return 0
 
 if __name__ == '__main__':
-    try:
-        sys.exit(main(sys.argv))
-    except Exception as e:
-        print('An exception occured:', type(e))
-        print('info:', e)
+    exit(main(sys.argv))
