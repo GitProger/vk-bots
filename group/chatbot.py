c2ebc07ec963b3d2afc8e31bd5a318d052c69fa7
GitProger <51924999+GitProger@users.noreply.github.com>
2020-02-25 00:38:33 +0300

    Update chatbot.py

--- group/chatbot.py
+++ group/chatbot.py
@@ -12,19 +12,23 @@ token = 'your_access_token'
 def cur_time():
     return datetime.datetime.now().strftime("%H:%M")
 
+def gen_table():
+    return {
+        "спасибо": "Пожалуйста",
+        "пока"   : "Пока(",
+    }
+
+def get_answer(text):
+    tab = gen_table()
+    for regexp in tab.keys():
+        if re.search(regexp, text):
+            return tab[regexp]
+    return "Не понял запроса."
 
 class MyBot(ChatbotBase):
     def log(self, event):
-        json = self.vk.users.get(user_ids=event.user_id, fields='')
-        inf = json[0]
-        n = inf['first_name'] + ' ' + inf['last_name']
-        print(event.user_id, " (" + n + "): ", event.text, sep="")
-    def send(self, uid, text):
-        self.vk.messages.send(
-            user_id = uid,
-            message = text,
-            random_id = random.randint(0, 2 ** 24),
-        )
+        i = event.user_id
+        print(i, " (", self.who_full(i), "): ", event.text, sep="")
 
     def parse(self, txt, conf):
         ptxt = txt.lower()
@@ -36,18 +40,37 @@ class MyBot(ChatbotBase):
                    Это тестовая версия бота, пока ты можешь только:
                     * спросить сколько времени,
                     * как тебя зовут
+                    * команда chg:
+                    --* По умолчанию выключена.
+                    --* Переключает режим.
+                    --* Если включена, то чтобы я ответил на сообщение 
+                    -- ко мне надо обратиться "Бот" или "Bot".
                    '''
         elif re.search("(как\s.*меня\s.*зовут|мо(е|ё).*\sимя|кто\s+я|я\s*кто)", ptxt):
             return "Тебя зовут " + uname + "."
-        else:
+        elif re.search("привет", ptxt):
             return "Привет, " + uname + "!"
-            
+        elif re.search("chg\.?$", ptxt):
+            self.mode ^= 1
+            return "Ок"
+        else:
+            return get_answer(ptxt)
+
+
     def process(self, event):
         if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
             self.log(event)
             uid = event.user_id
             txt = event.text
-            self.send(uid, self.parse(txt, event))            
+            if self.ap_mode:
+                if self.was_called(txt):
+                    self.send(uid, self.parse(txt, event))
+            else:
+                self.send(uid, self.parse(txt, event))
+
+    def __init__(self, tok):
+        ChatbotBase.__init__(self, tok)
+        self.ap_mode = 0
 
 bot = MyBot(token)
 bot.routine()
