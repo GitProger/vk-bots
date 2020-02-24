4456b4996a39fac4f1489fc706a90a73f50e2867
GitProger <51924999+GitProger@users.noreply.github.com>
2020-02-24 18:54:10 +0300

    Update chatbot.py

--- group/chatbot.py
+++ group/chatbot.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 
-import sys, os, requests, re
+import sys, os, requests, re, random
 import vk_api, json, time, datetime
 import math
+from vk_api.longpoll import VkLongPoll, VkEventType
 from rbase import ChatbotBase
 
 token = 'your_access_token'
 
-bot = ChatbotBase(token)
+class MyBot(ChatbotBase):
+    def process(self, event):
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+            print(event.user_id, " (" + self.who(event.user_id) + "): ", event.text, sep="")
+
+            self.vk.messages.send(
+                user_id = event.user_id,
+                message = 'Привет, ' + self.who(event.user_id),
+                random_id = random.randint(0, 2 ** 24),
+            )	
+
+bot = MyBot(token)
 bot.routine()
 
