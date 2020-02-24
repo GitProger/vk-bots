5ec8bfe78ef1ac014867522d62242020b0d049db
GitProger <51924999+GitProger@users.noreply.github.com>
2020-02-24 18:46:22 +0300

    Update rbase.py

--- group/rbase.py
+++ group/rbase.py
@@ -1,83 +1,19 @@
 #!/usr/bin/env python3
 
-import sys, os, requests, re
+import sys, os, requests, re, random
 import vk_api, json, time, datetime
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-class BasicSessionResponder:
-    def __init__(self, log, pas, tok):
-        self.__session = requests.Session()
-        self.vk = vk_api.VkApi(log, pas, tok)
-        self.vk.auth(token_only=True)
+
+def list_to_string(lst):
+    return ', '.join(map(str, lst))
+
+class ChatbotBase:
     def __init__(self, tok):
-        self.__session = requests.Session()
-        self.vk = vk_api.VkApi(tok)
-        self.vk.auth(token_only=True)
+        self.vk_sess = vk_api.VkApi(token=tok)
+        self.longpoll = VkLongPoll(self.vk_sess)
+        self.vk = self.vk_sess.get_api()
 
-    def online(self, uid):
-        inf = self._whois(uid, 'online')[0]
-        return inf['online']
-
-    def many_online(self, uids):
-        inf = self.vk.method(
-            'users.get',
-            {
-                'user_ids': ', '.join(map(str, uids)),
-                'fields': 'online',
-            }
-        )
-        res = dict()
-        for i in inf:
-            res[i['id']] = i['online']
-        return res
-
-    def whois(self, uid, fields=''):
-        return self.vk.method(
-            'users.get',
-            {
-                'user_ids': uid,
-                'fields': fields
-            }
-        )
-
-    def name(self, uid):
-        inf = self._whois(uid)[0]
-        return inf['first_name'] + ' ' + inf['last_name']
-
-    def friends(self, uid, fields=''):
-        return self.vk.method(
-            'friends.get',
-            {
-                'user_id': uid,
-                'fields': fields
-            }
-        )
-
-    def send(self, user_id, message):
-        return self.vk.method(
-            'messages.send', 
-            {
-                'user_id': user_id,
-                'random_id': 1, ## 
-                'message': message
-            }
-        )
-    def chatinfo(self, chat_id, fields=''): # doents work
-        return self.vk.method(
-            'messages.getChat', 
-            {
-                'chat_id': chat_id,
-                'fields': fields
-            }
-        )
-        
-
-class ChatbotBase(BasicSessionResponder):
-    def __init__(self, tok, names=["Bot"]):
-        BasicSessionResponder.__init__(self, tok)
-        self.longpoll = VkLongPoll(self.vk)
-        self.appeals = names
-		
     def was_called(self, message):
         for name in self.appeals:
             if bool(re.match("^" + name + "\s*,", message)):
@@ -85,14 +21,7 @@ class ChatbotBase(BasicSessionResponder):
         return False
 
     def solve_exc(self, e):
-        pass
-
-    def process(self, event):
-        if event.type == VkEventType.MESSAGE_NEW:
-            uid = event.used_id
-            txt = event.text
-            if self.was_called(txt) != -1:
-                self.send(uid, 'msg')
+        print("Error:", e)
 
     def routine(self):
         for event in self.longpoll.listen():
@@ -100,3 +29,34 @@ class ChatbotBase(BasicSessionResponder):
                 self.process(event)
             except Exception as e:
                 self.solve_exc(e)
+
+    def who(self, uid):
+        json = self.vk.users.get(user_ids=uid, fields='')
+        inf = json[0]
+        return inf['first_name'] #+ ' ' + inf['last_name']
+
+
+    def process(self, event):
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+            print(event.user_id, " (" + self.who(event.user_id) + "): ", event.text, sep="")
+
+
+            self.vk.messages.send(
+                user_id = event.user_id,
+                message = 'Привет, ' + self.who(event.user_id),
+                random_id = random.randint(0, 2 ** 24),
+            )
+
+            '''
+            if event.text.find('hi') != -1:
+                if event.from_user:
+                    self.vk.messages.send(
+                        user_id=event.user_id,
+                        message='Hello'
+		            )
+                elif event.from_chat:
+                    self.vk.messages.send(
+                        chat_id=event.chat_id,
+                        message='Hello'
+		            )
+            '''
